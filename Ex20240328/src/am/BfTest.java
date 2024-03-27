35725057ad128429b54ff7ed550079796e8c340e
wjsxorjs <wjsxorjs@naver.com>
2024-03-28 08:17:15 +0900

    JAVA 2024.03.28 0.0

---
+++ Ex20240328/src/am/BfTest.java
@@ -0,0 +1,10 @@
+package am;
+
+public class BfTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Ex20240328/src/pm/BfTest.java
@@ -0,0 +1,10 @@
+package pm;
+
+public class BfTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
